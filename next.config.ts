0af481a1216c1b2ea73840378d0ffc41245d22d3
mdrabil <ermdrabil@gmail.com>
2026-05-29 17:06:23 +0530

    rename Customizationsection to CustomizationSection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+
+  async headers() {
+    return [
+      {
+        source: "/:all*(js|css)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
